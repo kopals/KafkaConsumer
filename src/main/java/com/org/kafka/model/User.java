7717a88334a4bfcbb74abc852beeaabb31f2e093
Ashwin Rao <ashwinrao96@gmail.com>
2019-07-27 16:37:33 -0400

    checkin springbootkafka project

---
+++ src/main/java/com/org/kafka/model/User.java
@@ -0,0 +1,14 @@
+package com.org.kafka.model;
+
+public class User {
+	private String name;
+	private String email;
+	
+	public User(String name, String email)
+	{
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+}
