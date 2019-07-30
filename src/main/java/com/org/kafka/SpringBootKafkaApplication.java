cb0c5be8643271373ac248d3f0baa5a9c9b959f1
Ashwin Rao <ashwinrao96@gmail.com>
2019-07-29 22:23:25 -0400

    Spring boot Producer microservice

--- src/main/java/com/org/kafka/SpringBootKafkaApplication.java
+++ src/main/java/com/org/kafka/SpringBootKafkaApplication.java
@@ -2,6 +2,7 @@ package com.org.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @SpringBootApplication
 public class SpringBootKafkaApplication {
--- src/main/java/com/org/kafka/controller/KafkaController.java
+++ src/main/java/com/org/kafka/controller/KafkaController.java
@@ -1,26 +1,33 @@
 package com.org.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.org.kafka.producer.Producer;
+import com.org.kafka.model.User;
+
 
 @RestController
-@RequestMapping(value = "/kafka")
+@RequestMapping("kafka")
 public class KafkaController
 {
-	 private final Producer producer;
+	
 
-	    @Autowired
-	    KafkaController(Producer producer) {
-	        this.producer = producer;
-	    }
+	 @Autowired
+	    private KafkaTemplate<String, User> kafkaTemplate;
 
-	    @PostMapping(value = "/publish")
-	    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
-	        this.producer.sendMessage(message);
+	    private static final String TOPIC = "TopicProducer";
+
+	    @GetMapping("/publish/{name}")
+	    public String post(@PathVariable("name") final String name) {
+
+	        kafkaTemplate.send(TOPIC, new User(name, "kopal123@ymail.com"));
+
+	        return "Published successfully";
 	    }
 }
